4ce1fdfca799c64440450536bf908db980c2ab2e
Jef Ogues <jef.hotmail_@hotmail.com>
2022-07-04 21:02:02 -0400

    added sign up form

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ db.on('error', console.error.bind(console, 'mongo connection error'));
 const User = mongoose.model(
   "User",
   new Schema({
-    username: { type: String, required: true},
-    password: { type: String, required: true}
+    username: { type: String, required: true },
+    password: { type: String, required: true }
   })
 );
 
@@ -31,5 +31,18 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.render('index'));
+app.get('/sign-up', (req, res) => res.render('sign-up-form'));
+
+app.post("/sign-up", (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  }).save(err => {
+    if (err) { 
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 app.listen(3000, () => console.log('app listening on port 3000!'))
